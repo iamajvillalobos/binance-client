1d0993d35bd47919a3dbcb010cd80ce48ac78b2e
AJ Villalobos <hi@ajvillalobos.com>
2020-09-09 14:50:20 +0800

    build: initialize gem

---
+++ lib/binance/client.rb
@@ -0,0 +1,8 @@
+require "binance/client/version"
+
+module Binance
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/binance/client/version.rb
@@ -0,0 +1,5 @@
+module Binance
+  module Client
+    VERSION = "0.1.0"
+  end
+end
